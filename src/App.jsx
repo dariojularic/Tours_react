9260d23359faf68a48d452e97b178c69db8e7979
Dario Jularic <dariojularic0@gmail.com>
2024-09-08 10:27:32 +0200

    Create fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [tours, setTours] = useState();
+  const [tours, setTours] = useState(null);
+  const [error, setError] = useState(null);
+  const url = "https://www.course-api.com/react-tours-project";
+
+  // const a = async () => {
+  //   const response = await fetch(url)
+  //   console.log(response)
+  //   const data = await response.json();
+  //   console.log(data)
+  // }
+  // a()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        // console.log(response)
+        const json = await response.json();
+        // console.log(json)
+        setTours(json);
+        console.log(tours)
+        console.log(json)
+      } catch (error) {
+        setError(error)
+        console.log(error)
+      }
+    };
+    fetchData()
+  }, [])
 
   return (
     <>
       <div className="container">
         <h1>Our Tours</h1>
         <ul className="tours-list">
-          
+
         </ul>
       </div>
     </>
