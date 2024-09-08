85a8d95c2a279f4d8ee9be71df620f2d699a823f
Dario Jularic <dariojularic0@gmail.com>
2024-09-08 10:45:29 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,41 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import Tour from "./Tour"
 
 function App() {
   const [tours, setTours] = useState(null);
   const [error, setError] = useState(null);
   const url = "https://www.course-api.com/react-tours-project";
 
-  // const a = async () => {
-  //   const response = await fetch(url)
-  //   console.log(response)
-  //   const data = await response.json();
-  //   console.log(data)
-  // }
-  // a()
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
-        // console.log(response)
+        console.log(response)
         const json = await response.json();
         // console.log(json)
         setTours(json);
-        console.log(tours)
-        console.log(json)
+        // console.log(tours)
+        // console.log(json)
       } catch (error) {
         setError(error)
-        console.log(error)
+        // console.log(error)
       }
     };
     fetchData()
   }, [])
+
+  // console.log(tours)
 
   return (
     <>
       <div className="container">
         <h1>Our Tours</h1>
         <ul className="tours-list">
-
+          {tours.map(tour => {
+            console.log(tour)
+            return <Tour key={tour.id} {...tour}/>
+          })}
         </ul>
       </div>
     </>
--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,5 +1,19 @@
 import "./Tour.css"
 
-function Tour() {
-  
+function Tour(props) {
+  const {id, name, info, image, price} = props;
+  return (
+    <li>
+      <div>
+        <img src={image} alt="" />
+        <p>{price}</p>
+      </div>
+      <h3>{name}</h3>
+      <p>{info}</p>
+
+    </li>
+  )
+
 }
+
+export default Tour
