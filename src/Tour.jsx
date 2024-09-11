1317a6ea86897ce3655ce3f356a6c9fcfce0f61f
Dario Jularic <dariojularic0@gmail.com>
2024-09-11 13:25:28 +0200

    Add style

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -3,13 +3,13 @@ import "./Tour.css"
 function Tour(props) {
   const {id, name, info, image, price} = props;
   return (
-    <li>
-      <div>
-        <img src={image} alt="" />
-        <p>{price}</p>
+    <li className="list-item">
+      <div className="image-container">
+        <img className="image" src={image} alt="" />
+        <p className="price">{price}</p>
       </div>
-      <h3>{name}</h3>
-      <p>{info}</p>
+      <h3 className="tour-name">{name}</h3>
+      <p className="tour-info">{info}</p>
     </li>
   )
 
